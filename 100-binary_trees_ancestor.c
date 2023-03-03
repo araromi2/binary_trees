313b038827e65803e489e55f66e269a2eb7b3861
araromi2 <abdulhameedararomi@gmail.com>
2023-03-03 06:36:21 -0800

    Update 100-binary...

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,22 +7,22 @@
  *
  * Return: Pointer to the lowest common ancestor node, or NULL if not found
  */
-binary_tree_t *binary_trees_ancestor
-(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	      const binary_tree_t *second)
 {
 	if (!first || !second)
-		return NULL;
+		return (NULL);
 
 	/* Check if one of the nodes is the ancestor of the other */
 	if (first == second)
-		return (binary_tree_t *)first;
+		return ((binary_tree_t *)first);
 
 	/* Check if one of the nodes is the parent of the other */
 	if (first == second->parent)
-		return (binary_tree_t *)first;
+		return ((binary_tree_t *)first);
 	if (second == first->parent)
-		return (binary_tree_t *)second;
+		return ((binary_tree_t *)second);
 
 	/* Recursively search in the left and right subtrees */
-	return binary_trees_ancestor(first->parent, second->parent);
+	return (binary_trees_ancestor(first->parent, second->parent));
 }
