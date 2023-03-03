489345c9a1caaf4933fe9ee576877884e31389ab
araromi2 <abdulhameedararomi@gmail.com>
2023-03-03 06:07:48 -0800

    Fix betty doc error in 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,7 +3,8 @@
  * binary_tree_sibling - Finds the sibling of a node
  * @node: Pointer to the node to find the sibling
  *
- * Return: Pointer to the sibling node. If node is NULL or the parent is NULL, return NULL
+ * Return: Pointer to the sibling node. If node is NULL or 
+ * the parent is NULL, return NULL
  * If node has no sibling, return NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
